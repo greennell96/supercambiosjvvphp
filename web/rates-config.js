faf2c69a08dcd866e3c5eff7fe2e99dd548e5410
Jose Reyes <joseedreyes96@gmail.com>
2026-08-26 17:00:17 +0200

    Update currency exchange rates and date

--- web/rates-config.js
+++ web/rates-config.js
@@ -11,7 +11,7 @@
 // 3 = + tilde (completes the mark).
 window.SITE_CONFIG = {
   etapa: 2,
-  eurToVes: 970,
-  vesToEur: 1100,
-  updated: "24/08/2026"
+  eurToVes: 1000,
+  vesToEur: 1130,
+  updated: "26/08/2026"
 };
